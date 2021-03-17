d397aff6cbc87a2d999046efe6be8914b3d94c2f
Alfonso Lopez <alopez@citylendinginc.com>
2021-03-17 07:19:34 -0400

    Removing unnecessary files, Removin JavaScript Request for API, Adding Request only with PHP for SuperHero One

--- superheroe-one/modules/superheroe-one/includes/frontend.php
+++ superheroe-one/modules/superheroe-one/includes/frontend.php
@@ -11,24 +11,47 @@
  */
 
 
+if((isset($settings->superheroe_name)) && (!empty($settings->superheroe_name))){
+    $data=$module->apirequest($settings->superheroe_name);
+    //console.log($data);
+    if($data["response"]!="error")
+    {
+?>
+    <div class="card" style="width: 14rem;">
+        <div class="superheroe-image">
+        <img src="<?php echo $data["results"][0]["image"]["url"]; ?>" alt="SuperHero_picture"/>
+                    </div>
+        
+        <div class="card-body">
+        <h4 class="superheroe-name"><?php echo $data["results"][0]["name"]; ?></h4>
+        <p class="card-text">
+            <span class="intelligence">Intelligence: <?php echo $data["results"][0]["powerstats"]["intelligence"]; ?></span><br>
+            <span class="strength">strength: <?php echo $data["results"][0]["powerstats"]["strength"]; ?></span><br>
+            <span class="speed">speed: <?php echo $data["results"][0]["powerstats"]["speed"]; ?></span><br>
+            <span class="durability">durability: <?php echo $data["results"][0]["powerstats"]["durability"]; ?></span><br>
+            <span class="power">power: <?php echo $data["results"][0]["powerstats"]["power"]; ?></span><br>
+            <span class="combat">combat: <?php echo $data["results"][0]["powerstats"]["combat"]; ?></span><br>
+        </p>
+        </div>					  
+        <div class="card-body">
+        <div class="superheroe-biografy">
+            <p class="superheroe-biography-text">
+                <span class="full-name">Full Name: <?php echo $data["results"][0]["biography"]["full-name"]; ?></span><br>
+                <span class="alter-egos">Alter Egos: <?php echo $data["results"][0]["biography"]["alter-egos"]; ?></span><br>
+                <span class="City">City: <?php echo $data["results"][0]["work"]["base"]; ?></span><br>
+            </p>
+        </div>
+        </div>
+    </div>
+<?php
+    } else {
+        echo "<h3>Select a Valid Super Hero</h3>";
+    }
+} else {
+    echo "<h3>Please select a Super Hero</h3>";
+}
 
 ?>
 
-<div id="contenedor">
 
-
-    <div class="container">
-    
-        <div id="superheroes" class="row">
-        
-
-    
-        </div>
-    
-
-    </div>
-    
-
-
-</div>
 
--- superheroe-one/modules/superheroe-one/includes/loadSuperHeroes.php
+++ superheroe-one/modules/superheroe-one/includes/loadSuperHeroes.php
@@ -1,34 +1,7 @@
 <?php 
 
 
-if(isset($_GET['name'])){
 
-
-$ch = curl_init("https://superheroapi.com/api/10159786368926318/search/".$_GET['name']);
-
-curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-
-//set the content type to application/json
-
-//return response instead of outputting
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-
-//execute the POST request
-$result = curl_exec($ch);
-
-
-$data = json_decode($result, true);
-curl_close($ch);
-
-$payload = json_encode($data);
-
-print_r($payload);
-
-
-
-}
 
 
 
